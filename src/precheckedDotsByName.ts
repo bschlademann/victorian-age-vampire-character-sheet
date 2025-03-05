d5f26128543879727f819831b03775dde4efe27a
Benni-beep <krikelkr4kel@gmail.com>
2025-03-05 09:34:02 +0100

    feat: add preCheckedDotsByName

---
+++ src/precheckedDotsByName.ts
@@ -0,0 +1,24 @@
+const preCheckedDotsByName = {
+    "körperkraft": 1,
+    "geschick": 1,
+    "widerstand": 1,
+    "charisma": 1,
+    "manipulation": 1,
+    "erscheinung": 1,
+    "wahrnehmung": 1,
+    "intellgenz": 1,
+    "alertness": 1,
+    "brawl": 1,
+    "dodge": 2,
+    "firearms": 1,
+    "ride": 1,
+    "linguistics": 3,
+    "ally": 1,
+    "generation":5,
+    "status": 1,
+    "gewissen": 1,
+    "selbstbeherrschung": 1,
+    "mut": 1
+  }
+
+export default preCheckedDotsByName
